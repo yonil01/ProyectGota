8552b3e84959eb3b157e27d3e6e62a8a2efaadec
Juancxh <juanmcm2oo1@gmail.com>
2021-01-02 04:15:56 -0500

    Modified: login style

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import '../style.scss'
+import '../../assets/scss/reset.scss'
+import logo from '../../assets/images/logo.png'
 
 const Login = () => {
     const [DataForm, setDataForm] = useState({userName: '', password: ''});
@@ -20,30 +22,41 @@ const Login = () => {
         const {name, value} = event.target;
         setDataForm({...DataForm, [name]: value});
     };
+    // setTimeout(() => {  console.log("World!"); }, 2000);
     return (
         <div className="main">
-            <div className="container">
-                <div className="title"><h1>Gota</h1></div>
-                <div className="form">
-                    <form onSubmit={handleSubmit} onChange={handleChange}>
-                        <div>
-                            <input type="text" placeholder="Usuario" name='userName'/>
-                            <input type="password" placeholder="Contraseña" name='password'/>
-                        </div>
-                        <button>LOG IN</button>
-                        <br/>
-                        <Link to="/recovery" replace>Recuperar contraseña</Link>
-                        <br/>
-                        <Link to="/register" replace>Registrar</Link>
-                    </form>
-                </div>
+            <div className="screen-loader">
                 <div>
-                    <span className="txt">
-                        Sistema de Getios de productos
-                    </span>
+                    <img src={logo} alt="Gota"/>
                 </div>
             </div>
+            <div className="uiUser-container">
+            </div>
+            <div className="uiUser-container"></div>
         </div>
+        // <div className="main">
+        //     <div className="container">
+        //         <div className="title"><h1>Gota</h1></div>
+        //         <div className="form">
+        //             <form onSubmit={handleSubmit} onChange={handleChange}>
+        //                 <div>
+        //                     <input type="text" placeholder="Usuario" name='userName'/>
+        //                     <input type="password" placeholder="Contraseña" name='password'/>
+        //                 </div>
+        //                 <button>LOG IN</button>
+        //                 <br/>
+        //                 <Link to="/recovery" replace>Recuperar contraseña</Link>
+        //                 <br/>
+        //                 <Link to="/register" replace>Registrar</Link>
+        //             </form>
+        //         </div>
+        //         <div>
+        //             <span className="txt">
+        //                 Sistema de Getios de productos
+        //             </span>
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
 
